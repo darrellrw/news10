cdd96a37bf4286ecbaa483a4b712cb4a35481bee
carolineadicahya <11211026@student.itk.ac.id>
2023-11-09 21:59:43 +0800

    login-regist on progress

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,20 @@
-import './App.css'
-import { NavigationBar } from './components/NavigationBar'
-import { FooterBar } from './components/FooterBar'
+import "./App.css";
+import { NavigationBar } from "./components/NavigationBar";
+import { FooterBar } from "./components/FooterBar";
+// import { Login } from "./components/login";
+// import { Register } from "./components/register";
 
 function App() {
-  
   return (
+    // <div>
+    //   <Login></Login>
+    //   <Register></Register>
+    // </div>
     <div className="flex flex-col h-screen justify-between">
       <NavigationBar></NavigationBar>
       <FooterBar></FooterBar>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;

905fb90cbd7cdedaeec516f424979d074b8594f9
carolineadicahya <11211026@student.itk.ac.id>
2023-11-26 21:47:08 +0800

    register-login. ok.

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,36 @@
-import "./App.css";
-import { NavigationBar } from "./components/NavigationBar";
-import { FooterBar } from "./components/FooterBar";
+// import "./App.css";
+// // import { NavigationBar } from "./components/NavigationBar";
+// // import { FooterBar } from "./components/FooterBar";
 // import { Login } from "./components/login";
 // import { Register } from "./components/register";
 
-function App() {
+// function App() {
+//   return (
+//     <div>
+//       <Login></Login>
+//       <Register></Register>
+//     </div>
+//     // <div className="flex flex-col h-screen justify-between">
+//     //   <NavigationBar></NavigationBar>
+//     //   <FooterBar></FooterBar>
+//     // </div>
+//   );
+// }
+
+// export default App;
+
+// ............................
+import React from "react";
+import Login from "./components/login"; // Sesuaikan path dengan struktur proyek Anda
+import Register from "./components/register";
+
+const App = () => {
   return (
-    // <div>
-    //   <Login></Login>
-    //   <Register></Register>
-    // </div>
-    <div className="flex flex-col h-screen justify-between">
-      <NavigationBar></NavigationBar>
-      <FooterBar></FooterBar>
+    <div>
+      {/* <Login /> */}
+      <Register />
     </div>
   );
-}
+};
 
 export default App;

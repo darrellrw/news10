9ade3b6a0597196fb09c5e1296a1a3df60324184
DwikoFebrian <11211034@student.itk.ac.id>
2023-11-29 08:15:04 +0800

    perubahan

---
+++ src/components/AddComment.jsx
@@ -0,0 +1,109 @@
+import React from "react";
+
+const AddComment = (props) => {
+  return (
+    <div className="box-border flex justify-start items-start flex-grow-0 flex-shrink-0 w-[1431px] h-[678px]">
+      <div className="box-border flex justify-start items-start flex-grow-0 flex-shrink-0 w-[1431px] h-[678px] overflow-hidden gap-20 px-16 py-28 bg-[#de5959]">
+        <div className="box-border flex flex-col justify-start items-start flex-grow basis-full gap-6">
+          <div className="box-border flex flex-col justify-start items-start self-stretch flex-grow-0 flex-shrink-0 relative gap-2">
+            <p className="whitespace-pre-wrap flex-grow-0 flex-shrink-0 self-stretch leading-[1.5] text-left text-black">
+              <span className="whitespace-pre-wrap flex-grow-0 flex-shrink-0 self-stretch font-['Roboto'] text-[32px] leading-[1.5] font-medium text-left text-black">
+                A
+              </span>
+              <span className="whitespace-pre-wrap flex-grow-0 flex-shrink-0 self-stretch font-['Roboto'] text-[32px] leading-[1.5] font-medium text-left text-black">
+                d
+              </span>
+              <span className="whitespace-pre-wrap flex-grow-0 flex-shrink-0 self-stretch font-['Roboto'] text-[32px] leading-[1.5] font-medium text-left text-black">
+                d
+              </span>
+              <span className="whitespace-pre-wrap flex-grow-0 flex-shrink-0 self-stretch font-['Roboto'] text-[32px] leading-[1.5] font-medium text-left text-black">
+                {" "}
+              </span>
+              <span className="whitespace-pre-wrap flex-grow-0 flex-shrink-0 self-stretch font-['Roboto'] text-[32px] leading-[1.5] font-medium text-left text-black">
+                A
+              </span>
+              <span className="whitespace-pre-wrap flex-grow-0 flex-shrink-0 self-stretch font-['Roboto'] text-[32px] leading-[1.5] font-medium text-left text-black">
+                {" "}
+              </span>
+              <span className="whitespace-pre-wrap flex-grow-0 flex-shrink-0 self-stretch font-['Roboto'] text-[32px] leading-[1.5] font-medium text-left text-black">
+                C
+              </span>
+              <span className="whitespace-pre-wrap flex-grow-0 flex-shrink-0 self-stretch font-['Roboto'] text-[32px] leading-[1.5] font-medium text-left text-black">
+                o
+              </span>
+              <span className="whitespace-pre-wrap flex-grow-0 flex-shrink-0 self-stretch font-['Roboto'] text-[32px] leading-[1.5] font-medium text-left text-black">
+                m
+              </span>
+              <span className="whitespace-pre-wrap flex-grow-0 flex-shrink-0 self-stretch font-['Roboto'] text-[32px] leading-[1.5] font-medium text-left text-black">
+                m
+              </span>
+              <span className="whitespace-pre-wrap flex-grow-0 flex-shrink-0 self-stretch font-['Roboto'] text-[32px] leading-[1.5] font-medium text-left text-black">
+                e
+              </span>
+              <span className="whitespace-pre-wrap flex-grow-0 flex-shrink-0 self-stretch font-['Roboto'] text-[32px] leading-[1.5] font-medium text-left text-black">
+                n
+              </span>
+              <span className="whitespace-pre-wrap flex-grow-0 flex-shrink-0 self-stretch font-['Roboto'] text-[32px] leading-[1.5] font-medium text-left text-black">
+                t
+              </span>
+              <span className="whitespace-pre-wrap flex-grow-0 flex-shrink-0 self-stretch font-['Roboto'] text-[32px] leading-[1.5] font-medium text-left text-black">
+                {" "}
+              </span>
+              <span className="whitespace-pre-wrap flex-grow-0 flex-shrink-0 self-stretch font-['Roboto'] text-[32px] leading-[1.5] font-medium text-left text-black">
+                :
+              </span>
+              <span className="whitespace-pre-wrap flex-grow-0 flex-shrink-0 self-stretch font-['Roboto'] text-[32px] leading-[1.5] font-medium text-left text-black">
+                {" "}
+              </span>
+              <br />
+              <br />
+              <br />
+              <span className="whitespace-pre-wrap flex-grow-0 flex-shrink-0 self-stretch font-['Roboto'] text-base leading-[1.5] text-left text-black">
+                N
+              </span>
+              <span className="whitespace-pre-wrap flex-grow-0 flex-shrink-0 self-stretch font-['Roboto'] text-base leading-[1.5] text-left text-black">
+                a
+              </span>
+              <span className="whitespace-pre-wrap flex-grow-0 flex-shrink-0 self-stretch font-['Roboto'] text-base leading-[1.5] text-left text-black">
+                m
+              </span>
+              <span className="whitespace-pre-wrap flex-grow-0 flex-shrink-0 self-stretch font-['Roboto'] text-base leading-[1.5] text-left text-black">
+                e
+              </span>
+            </p>
+            <div className="box-border flex justify-start items-center self-stretch flex-grow-0 flex-shrink-0 h-12 gap-2 p-3 rounded-[5px] bg-white border border-black" />
+          </div>
+          <div className="box-border flex flex-col justify-start items-start self-stretch flex-grow-0 flex-shrink-0 relative gap-2">
+            <p className="whitespace-pre-wrap flex-grow-0 flex-shrink-0 self-stretch font-['Roboto'] text-base leading-[1.5] text-left text-black">
+              Email
+            </p>
+            <div className="box-border flex justify-start items-center self-stretch flex-grow-0 flex-shrink-0 h-12 gap-2 p-3 rounded-[5px] bg-white border border-black" />
+          </div>
+          <div className="box-border flex flex-col justify-start items-start self-stretch flex-grow-0 flex-shrink-0 relative gap-2">
+            <p className="whitespace-pre-wrap flex-grow-0 flex-shrink-0 self-stretch font-['Roboto'] text-base leading-[1.5] text-left text-black">
+              Message
+            </p>
+            <div className="box-border flex justify-start items-start self-stretch flex-grow-0 flex-shrink-0 h-[180px] relative p-3 rounded-[5px] bg-white border border-black">
+              <p className="whitespace-pre-wrap flex-grow font-['Roboto'] text-base leading-[1.5] text-left text-[#505050]">
+                Type your message...
+              </p>
+            </div>
+          </div>
+          <div className="box-border flex justify-start items-center flex-grow-0 flex-shrink-0 relative gap-3 pb-4">
+            <div className="box-border block flex-grow-0 flex-shrink-0 w-[18px] h-[18px] relative overflow-hidden bg-white border border-black" />
+            <p className="whitespace-pre-wrap flex-grow-0 flex-shrink-0 font-['Roboto'] text-sm leading-[1.5] text-left text-black">
+              I accept the Terms
+            </p>
+          </div>
+          <div className="box-border flex justify-center items-center flex-grow-0 flex-shrink-0 relative gap-2 px-6 py-3 rounded-[5px] bg-black border border-black">
+            <p className="whitespace-pre-wrap flex-grow-0 flex-shrink-0 font-['Roboto'] text-base leading-[1.5] text-left text-white">
+              Submit
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default AddComment;

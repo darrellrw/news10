b2d5dff334fc815d3d736d1f9d3204b667824245
darrellrw <11211028@student.itk.ac.id>
2023-12-02 22:29:32 +0800

    Master

--- src/pages/SavedPost.jsx
+++ src/pages/SavedPost.jsx
@@ -54,7 +54,7 @@ function SavedPost() {
                             records.map((news, index) => {
                                 return (
                                     <>
-                                        <form key={index} onSubmit={deletePost(index)} className="flex justify-content">
+                                        <form key={index} onSubmit={deletePost(index)} className="md:flex justify-content">
                                             <ArticleCard data={news}/>
                                             <input type="submit" value="Delete" className="text-white bg-red-700 hover:bg-red-800 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2"/>
                                         </form>
